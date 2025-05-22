dcb68330a254ab869381da2d4232e7550538beff
s4jeherr <s4jeherr@uni-trier.de>
2025-05-22 09:40:55 +0200

    Update llm_config.py

--- fopra/project/config/llm_config.py
+++ fopra/project/config/llm_config.py
@@ -3,8 +3,8 @@
 LLM_SERVER = {
     "hostname": "gpu.wi2.uni-trier.de",
     "ip": "136.199.130.136",
-    "username": "s4jeherr",
-    "password": "safepw",
+    "username": "",
+    "password": "",
     "quota": "100GB",
     "expiration": "2025-05-01"
 }
